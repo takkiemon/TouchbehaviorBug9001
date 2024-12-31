e0be03898f887a18bef6875958de972bc5a83b3e
takmanfa <takman@facilityapps.com>
2024-12-31 10:09:55 +0100

    add project

---
+++ TouchbehaviorBackgroundColors communitytoolkit 9001/TouchbehaviorBackgroundColors/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TouchbehaviorBackgroundColors
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public bool Selected { get; set; } = false;
+        public Color StatusBackgroundColor => Selected ? Colors.Yellow : Colors.Purple;
+        public Color ReverseStatusBackgroundColor => Selected ? Colors.MediumPurple : Colors.LightGoldenrodYellow;
+        public ObservableCollection<TestListItem> ItemListSource { get; set; } = new ObservableCollection<TestListItem>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            FillList();
+            BindingContext = new TestListItem()
+            {
+                Text = "Test item 1"
+            };
+        }
+
+        public void FillList()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                ItemListSource.Add(new TestListItem()
+                {
+                    Text = $"Test item {i + 1}",
+                    mainPage = this
+                });
+            }
+        }
+    }
+
+    public class TestListItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Color StatusBackgroundColor => Selected ? Colors.Yellow : Colors.Purple;
+        public Color ReverseStatusBackgroundColor => Selected ? Colors.MediumPurple : Colors.LightGoldenrodYellow;
+        public bool Selected { get; set; } = false;
+        public string IsSelectedText => Selected ? "Selected is set to true" : "Selected is set to false";
+        public MainPage mainPage { get; set; }
+        public string Text {  get; set; }
+        public Command TestTapCommand => new Command(() =>
+        {
+            Selected = true;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StatusBackgroundColor)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ReverseStatusBackgroundColor)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelectedText)));
+        });
+        public Command TestLongPressCommand => new Command(() =>
+        {
+            Selected = false;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StatusBackgroundColor)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ReverseStatusBackgroundColor)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelectedText)));
+        });
+        public Command ToggleCommand => new Command(() =>
+        {
+            Selected = !Selected;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StatusBackgroundColor)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ReverseStatusBackgroundColor)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelectedText)));
+        });
+    }
+}
---
+++ TouchbehaviorBackgroundColors communitytoolkit 9001/TouchbehaviorBackgroundColors/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TouchbehaviorBackgroundColors
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
